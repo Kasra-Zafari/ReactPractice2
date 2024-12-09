175e18a8d49cfa35a82e4c6a546848279a87b91f
Kasra Zafari <160012548+Kasra-Zafari@users.noreply.github.com>
2024-12-09 22:17:14 +0330

    footer

--- src/componets/Footer/index.jsx
+++ src/componets/Footer/index.jsx
@@ -1,8 +1,41 @@
 import classes from "./index.module.css"
+import React from "react"
+import { FaInstagram, FaLinkedin, FaGithub, FaYoutube} from "react-icons/fa";
 
 const Footer = () => {
-    return(
-        <h1 className={classes.footer}>footer</h1>
+    return (
+        <footer>
+            <div className={classes.logo}>
+                <h3>COMPANY</h3>
+                <ul>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">About Us</a></li>
+                </ul>
+            </div>
+            <div className={classes.company}>
+                <h3>ACCOUNT</h3>
+                <ul>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Order</a></li>
+                    <li><a href="#">Favorit</a></li>
+                    <li><a href="#">Basket</a></li>
+                </ul>
+            </div>
+            <div className={classes.account}>
+                <h3>FOLLOW US</h3>
+                <ul>
+                    <li><a href="#"><FaGithub className={classes.icon} /></a></li>
+                    <li><a href="#"><FaLinkedin className={classes.icon} /></a></li>
+                    <li><a href="#"><FaYoutube className={classes.icon} /></a></li>
+                    <li><a href="#"><FaInstagram className={classes.icon} /></a></li>
+                </ul>
+            </div>
+            <div className={classes.follow}>
+                <img src="logo.png" alt="" />
+            </div>
+        </footer>
     )
 }
 
