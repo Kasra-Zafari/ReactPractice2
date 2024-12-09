ba3f8f94a993c723c51a8d5b531d3db0b065dec6
Kasra Zafari <160012548+Kasra-Zafari@users.noreply.github.com>
2024-12-09 20:51:35 +0330

    card hover

--- src/componets/ProcuctCard/index.jsx
+++ src/componets/ProcuctCard/index.jsx
@@ -1,20 +1,20 @@
-import classee from "./index.module.css"
+import classes from "./index.module.css"
 
 const ProductCard = (props) => {
     const { id, title, price, description, category, image, rating, rate, count } = props
     return (
         <a href="#">
-            <div className={classee.card}>
-            <div className={classee.img}>
-            <img src={image} alt="" width="120px" />
+            <div className={classes.card}>
+                <div className={classes.img}>
+                    <img src={image} alt="" width="120px" />
+                </div>
+                <div className={classes.caption}>
+                    <h5 style={{ height: "50px" }}>{title}</h5>
+                    <p>{category}</p>
+                    <p>{price}$</p>
+                    <p>Rate: {rate}</p>
+                </div>
             </div>
-            <div className={classee.caption}>
-            <h5 style={{height:"70px"}}>{title}</h5>
-            <p>{category}</p>
-            <p>{price}$</p>
-            <p>Rate: {rate}</p>
-            </div>
-        </div>
         </a>
     )
 }
