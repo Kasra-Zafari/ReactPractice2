c3c5941d74955993d2654a8a4274f6976be4f5a4
Kasra Zafari <160012548+Kasra-Zafari@users.noreply.github.com>
2024-12-09 20:44:51 +0330

    card design

--- src/componets/ProcuctCard/index.jsx
+++ src/componets/ProcuctCard/index.jsx
@@ -3,13 +3,19 @@ import classee from "./index.module.css"
 const ProductCard = (props) => {
     const { id, title, price, description, category, image, rating, rate, count } = props
     return (
-        <div className={classee.card}>
+        <a href="#">
+            <div className={classee.card}>
+            <div className={classee.img}>
             <img src={image} alt="" width="120px" />
-            <h4>{title}</h4>
+            </div>
+            <div className={classee.caption}>
+            <h5 style={{height:"70px"}}>{title}</h5>
             <p>{category}</p>
             <p>{price}$</p>
             <p>Rate: {rate}</p>
+            </div>
         </div>
+        </a>
     )
 }
 export default ProductCard
